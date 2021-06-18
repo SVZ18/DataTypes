d4da18df7bdfb0cae773e89066bd796e8755efa9
Sandija Veze <sandijaveze@inbox.lv>
2021-06-18 20:40:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //integers
+        //These are the variables I declare
+        int firstNumber;
+        int secondNumber;
+        firstNumber = 5;
+        secondNumber = 6;
+
+        int sum = firstNumber + secondNumber;
+//        System.out.println(sum);
+
+        float firstFloat= 0.5f;
+        float secondFloat = 2.3f;
+
+        float floatSum = firstFloat + secondFloat;
+//        System.out.println(floatSum/4);
+
+        //doubles
+        double firstDouble = 0.7;
+        double secondDouble = 2.4 ;
+        double doubleSum = firstDouble + secondDouble;
+
+//        System.out.println(doubleSum);
+
+        String firstString = "Ice Cream";
+        String secondString = "Soya";
+
+        firstString = "Grape Juice";
+
+        System.out.println("I love" + firstString + "and" + secondString);
+
+
+
+
+
+    }
+}
